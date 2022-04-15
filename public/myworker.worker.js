00ef1d942df97441ae57b881eda672dd39b52d45
Brian Humphreys <humbfrancis@gmail.com>
2022-04-14 21:20:42 -0500

    worker to initialize worker created. worker added to public folder

---
+++ public/myworker.worker.js
@@ -0,0 +1,3 @@
+console.log("worker initialized");
+
+self.postMessage({eventData: "INITIALIZED"});
--- src/store/workers/workerSlice.ts
+++ src/store/workers/workerSlice.ts
@@ -1,44 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 // State of workers
 export enum WorkerState {
-    NOT_INITIALIZED = 'NOT_INITIALIZED',
-    STOPPED = 'STOPPED',
-    RUNNING = 'RUNNING'
+  NOT_INITIALIZED = "NOT_INITIALIZED",
+  READY = "READY",
+  RUNNING = "RUNNING",
+  TERMINATED = "TERMINATED",
 }
 
 // The interface describing the worker slice
 export interface WorkerSlice {
-    worker: WorkerState;
+  worker: WorkerState;
 }
 
 // Initial state object
 const initialWorkerSliceState: WorkerSlice = {
-    worker: WorkerState.NOT_INITIALIZED,
-}
+  worker: WorkerState.NOT_INITIALIZED,
+};
 
 // Slice creator
 const workerSlice = createSlice({
-    name: 'workers',
-    initialState: initialWorkerSliceState,
-    reducers: {
-        setWorkerStopped: (state: WorkerSlice) =>({
-            ...state,
-            worker: WorkerState.STOPPED,
-        }),
-        setWorkerRunning: (state: WorkerSlice) =>({
-            ...state,
-            worker: WorkerState.RUNNING,
-        }),
-        setWorkerNotInitialized: (state: WorkerSlice) =>({
-            ...state,
-            worker: WorkerState.NOT_INITIALIZED,
-        }),
-    },
+  name: "workers",
+  initialState: initialWorkerSliceState,
+  reducers: {
+    setWorkerReady: (state: WorkerSlice) => ({
+      ...state,
+      worker: WorkerState.READY,
+    }),
+    setWorkerRunning: (state: WorkerSlice) => ({
+      ...state,
+      worker: WorkerState.RUNNING,
+    }),
+    setWorkerNotInitialized: (state: WorkerSlice) => ({
+      ...state,
+      worker: WorkerState.NOT_INITIALIZED,
+    }),
+    setWorkerTerminated: (state: WorkerSlice) => ({
+      ...state,
+      worker: WorkerState.TERMINATED,
+    }),
+  },
 });
 
 // export the actions to set the state of our
-export const { setWorkerStopped, setWorkerRunning, setWorkerNotInitialized } = workerSlice.actions
+export const {
+  setWorkerReady,
+  setWorkerRunning,
+  setWorkerNotInitialized,
+  setWorkerTerminated,
+} = workerSlice.actions;
 
 // export reducer to plug into our state provider
-export default workerSlice.reducer
+export default workerSlice.reducer;
