cf02140c06eaf433aff0610f5208cddd294d7b62
Brian Humphreys <humbfrancis@gmail.com>
2022-04-18 15:28:33 -0500

    start template for canvas point workers. part 3 tutorial

---
+++ src/hooks/useWorkerInvoker.ts
@@ -0,0 +1,15 @@
+import { useCallback } from "react";
+import { Pos } from "../types";
+
+
+// you must work through part 1 AND part 2 tutorials to 
+// get to this point.  alternatively you can pull this branch
+// and start following along now!
+const useWorkerInvoker = (points: Pos[]) => {
+    return useCallback(() => {
+
+
+    }, [points.length]);
+}
+
+export default useWorkerInvoker;
