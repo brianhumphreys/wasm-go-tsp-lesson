388e80a9b51d5e7596917c51f4ffc9c3b42930fc
Brian Humphreys <humbfrancis@gmail.com>
2022-04-18 17:06:14 -0500

    adds comments about type conversion into the generic worker manager hook on the starting template.  next section we simply want to change the name of the hook to useNumberWorkerInvoker and build up from there

--- src/hooks/useWorkerInvoker.ts
+++ src/hooks/useWorkerInvoker.ts
@@ -1,6 +1,11 @@
 import { Pos } from "../types";
 
 // setup template
+// This hook will be used to handle inputs and outputs
+// of our generic useWorkerManager hook.  
+// first we want to change the name from useWorkerInvoker to
+// useNumberWorkerInvoker because we will be passing a number
+// back and forth between our worker to get warmed up.
 const useWorkerInvoker = (points: Pos[]) => {
 
 }
