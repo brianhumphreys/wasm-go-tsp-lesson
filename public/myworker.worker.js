3362ab3c6e326fc3a1b3dec1f8dde437ae260bb6
Brian Humphreys <humbfrancis@gmail.com>
2022-04-18 23:39:06 -0500

    add wasm_exec, go file with distance function and modifies web worker init function to initialize go wasm module

--- public/myworker.worker.js
+++ public/myworker.worker.js
@@ -1,26 +1,38 @@
+const initialize = (cb) => {
+  console.log("initializing worker");
 
+  // adds initialization code.  We need to add wasm_exec.js to our public folder
+  importScripts("/wasm_exec.js");
 
-const initialize = () => {
-    console.log('initializing worker');
-}
+  // after fetching the wasm script, we need to instanciate the go module and then
+  // fetch our transpiled go script
+  const go = new Go();
+  WebAssembly.instantiateStreaming(fetch("../test.wasm"), go.importObject).then(
+    (result) => {
+      console.log("finished loading Go script");
+      cb();
+    }
+  );
+};
 
 self.onmessage = (event) => {
-    const {eventData, eventType} = event.data;
+  const { eventData, eventType } = event.data;
 
-    if (eventType == "START_INITIALIZATION") {
-        initialize();
-        self.postMessage({ eventType: "INITIALIZED", })
-    }
+  if (eventType == "START_INITIALIZATION") {
+    // since the initialization is async because we fetch go scripts,
+    // lets pass the postMessage function as a call back that invoked once
+    // init is finished
+    initialize(() => self.postMessage({ eventType: "INITIALIZED" }));
+  }
 
-    if (eventType == "START") {
-        // print out the points array to verify that we are correctly passing 
-        // our data to the worker thread
-        console.log('worker!!!');
-        console.log(eventData);
+  if (eventType == "START") {
+    // print out the points array to verify that we are correctly passing
+    // our data to the worker thread
+    console.log("worker!!!");
+    console.log(eventData);
 
-        // pass the points array back to verify that we are correctly returning
-        // data from the worker thread back to the main thread
-        self.postMessage({ eventType: "FINISH", eventData });
-    }
-    
-}
+    // pass the points array back to verify that we are correctly returning
+    // data from the worker thread back to the main thread
+    self.postMessage({ eventType: "FINISH", eventData });
+  }
+};
--- src/hooks/usePointsArrayWorkerInvoker.ts
+++ src/hooks/usePointsArrayWorkerInvoker.ts
@@ -2,30 +2,11 @@ import { useCallback, useEffect } from "react";
 import { Pos } from "../types";
 import useWorkerManager from "./useWorkerManager";
 
-const usePointsArrayWorkerInvoker = (points: Pos[], ) => {
-    // we want to keep the useWorker Manager Separate from 
-    // the code that passes in the task data because we want the logic
-    // to be separate from the data is passing to the worker.  We see here 
-    // that we need to run a useCallback that listens to the length of the
-    // points array.  We do not want this dependency on points.length in the 
-    // manager because we might later want to run a worker that takes a map 
-    // of points (which we will need when running wasm golang)
-    const [taskResult, runWorker] = useWorkerManager<Pos[], Pos[]>();
-    // alternatively we can just specify
-    // const [taskResult, runWorker] = useWorkerManager<Pos[]>();
-    // as the generic type because the R template will default to Pos[] if it
-    // isn't explicitly stated what the output type will be. In this case, 
-    // both the input and output of the worker task will be Pos[] so we can 
-    // just say <Pos[]>  
-    useEffect(() => {
-        console.log('result!!!!');
-        console.log(taskResult);
-    }, [taskResult])
+const usePointsArrayWorkerInvoker = (points: Pos[], cb: (output: Pos[] | null) => void) => {
+    const runWorker = useWorkerManager<Pos[], Pos[]>(cb);
     return useCallback(() => {
         runWorker(points);
     }, [points.length]);
-
-
 }
 
 export default usePointsArrayWorkerInvoker;
