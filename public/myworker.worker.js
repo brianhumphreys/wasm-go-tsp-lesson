6ddafd3c3188521dec3a6161eb596dd2fea58589
Brian Humphreys <humbfrancis@gmail.com>
2022-04-19 18:01:37 -0500

    go wasm working initialized from worker

--- public/myworker.worker.js
+++ public/myworker.worker.js
@@ -1,82 +1,33 @@
 importScripts("/wasm_exec.js");
 
+function createWasmArray (array) {
+    const arrayMap = {};
+    for (let i =  0; i < array.length; i++) {
+        arrayMap[`${i}`] = array[i];
+    }
+    arrayMap['length'] = array.length;
+    console.log(arrayMap);
+    return arrayMap;
+}
 
-// https://blog.suborbital.dev/foundations-wasm-in-golang-is-fantastic
+function twoOpt() {
+
+    // console.log(self.global.distance([10, 10], [13, 14]));
+
+    console.log('yup');
+    // const array = [{x: 10, y: 10}, {x: 13, y: 14}, {x: 45, y: 17}, {x: 18, y: 5}, {x: 8, y: 18}, {x: 44, y: 12}, {x: 45, y: 3}, {x: 54, y: 31}, {x: 65, y: 36}, {x: 9, y: 18}]
+    // console.log(self.global.twoOpt(createWasmArray(array)));
+    
+}
+
 const go = new Go();
-if (!WebAssembly.instantiateStreaming) { 
-  WebAssembly.instantiateStreaming = async (resp, importObject) => {
-      const source = await (await resp).arrayBuffer()
-      const result = await WebAssembly.instantiate(source, importObject)
-      console.log()
-  }
-}
-function loadWasm(path) {
-  const go = new Go()
+WebAssembly.instantiateStreaming(
+    fetch("./gomodule.wasm"),
+    go.importObject
+  ).then((result) => {
+      console.log("faclk")
+      go.run(result.instance);
+      twoOpt();
+      
+  });
 
-  return new Promise((resolve, reject) => {
-    WebAssembly.instantiateStreaming(fetch(path), go.importObject)
-    .then(result => {
-      go.run(result.instance)
-      resolve(result.instance)
-    })
-    .catch(error => {
-      reject(error)
-    })
-  })
-}
-
-loadWasm("./gomodule.wasm").then(wasm => {
-  console.log("gomodule.wasm is loaded 👋")
-  // const output = self.global.add3(3);
-  // console.log("worker output: ", output);
-}).catch(error => {
-  console.log("ouch", error)
-}) 
-
-const initialize = (cb) => {
-  console.log("initializing worker");
-
-  // adds initialization code.  We need to add wasm_exec.js to our public folder
-  // importScripts("/wasm_exec.js");
-
-  // // after fetching the wasm script, we need to instanciate the go module and then
-  // // fetch our transpiled go script
-  // const go = new Go();
-  // WebAssembly.instantiateStreaming(fetch("./gomodule.wasm"), go.importObject).then(
-  //   (result) => {
-  //     go.run(result.instance);
-  //     console.log("finished loading Go script");
-
-  //     const output = self.global.add3(3);
-
-  //     console.log("worker output: ", output);
-  //     cb();
-  //   }
-  // );
-};
-
-self.onmessage = (event) => {
-  const { eventData, eventType } = event.data;
-
-  if (eventType == "START_INITIALIZATION") {
-    // since the initialization is async because we fetch go scripts,
-    // lets pass the postMessage function as a call back that invoked once
-    // init is finished
-    initialize(() => self.postMessage({ eventType: "INITIALIZED" }));
-  }
-
-  if (eventType == "START") {
-    // print out the input to verify that we are correctly passing
-    // our data to the worker thread
-    console.log("worker input: ", eventData);
-
-    console.log(self.global);
-    // invoke golang method
-    const result = self.global.add3(eventData);
-
-    console.log("worker output: ", result);
-
-    // pass the result back to the main thread
-    self.postMessage({ eventType: "FINISH", eventData: result });
-  }
-};
--- src/wasm_modules/main.go
+++ src/wasm_modules/main.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
-// func add3(input int) int {
-
-// 	return input + 3
-// }
-
-// func add3Wrapper() js.Func {
-// 	add3Function := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-// 		if len(args) != 1 {
-// 			return "Invalid number of arguments passed.  Expecting 1."
-// 		}
-
-// 		input := args[0].Index(0).Int()
-
-// 		fmt.Printf("wasm input: %d\n", input))
-
-// 		result := add3(input)
-
-// 		fmt.Printf("wasm output: %d\n", result))
-
-// 		return result
-// 	})
-// 	return add3Function
-// }
-
 func main() {
-
-	// js.Global().Set("add3", add3Wrapper())
-	// <-make(chan bool)
-	fmt.Println("👋 Hello World 🌍")
+  fmt.Println("👋 Hello World 🌍")
     // Prevent the function from returning, which is required in a wasm module
     <-make(chan bool)
 }
