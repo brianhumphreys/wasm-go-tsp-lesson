5858434ad6710e552aa8c283d8b891886d0fa66e
Brian Humphreys <humbfrancis@gmail.com>
2022-04-19 08:38:22 -0500

    updates canvas and worker invoker

--- src/hooks/usePointsArrayWorkerInvoker.ts
+++ src/hooks/usePointsArrayWorkerInvoker.ts
@@ -2,7 +2,7 @@ import { useCallback, useEffect } from "react";
 import { Pos } from "../types";
 import useWorkerManager from "./useWorkerManager";
 
-const usePointsArrayWorkerInvoker = (points: Pos[], ) => {
+const usePointsArrayWorkerInvoker = (points: Pos[], cb: (taskResult: Pos[] | null) => void) => {
     // we want to keep the useWorker Manager Separate from 
     // the code that passes in the task data because we want the logic
     // to be separate from the data is passing to the worker.  We see here 
@@ -10,17 +10,13 @@ const usePointsArrayWorkerInvoker = (points: Pos[], ) => {
     // points array.  We do not want this dependency on points.length in the 
     // manager because we might later want to run a worker that takes a map 
     // of points (which we will need when running wasm golang)
-    const [taskResult, runWorker] = useWorkerManager<Pos[], Pos[]>();
+    const runWorker = useWorkerManager<Pos[], Pos[]>(cb);
     // alternatively we can just specify
     // const [taskResult, runWorker] = useWorkerManager<Pos[]>();
     // as the generic type because the R template will default to Pos[] if it
     // isn't explicitly stated what the output type will be. In this case, 
     // both the input and output of the worker task will be Pos[] so we can 
     // just say <Pos[]>  
-    useEffect(() => {
-        console.log('result!!!!');
-        console.log(taskResult);
-    }, [taskResult])
     return useCallback(() => {
         runWorker(points);
     }, [points.length]);
