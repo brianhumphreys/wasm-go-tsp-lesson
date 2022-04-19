7d66fb0c232dec8aab9cc07434deb37fb4500327
Brian Humphreys <humbfrancis@gmail.com>
2022-04-18 23:12:58 -0500

    update worker manager

--- src/hooks/useWorkerManager.ts
+++ src/hooks/useWorkerManager.ts
@@ -1,23 +1,18 @@
 import { useEffect, useState } from "react";
 import WorkerManager from "../workers/WorkerManager";
 
-// we need to doctor our worker manager to accept an array of points
-const useWorkerManager = <T, R = T>(): [R | null, (taskData: T) => void] => {
+const useWorkerManager = <T, R = T>(cb: (taskResult: R | null) => void): ((taskData: T) => void) => {
   const [worker1] = useState(new WorkerManager("myworker.worker.js"));
-
-  const [taskResult, setTaskResult] = useState<R | null>(null);
 
   useEffect(() => {
     worker1.initialize();
   });
 
-  const runWorker = (taskData: T) => {
-    worker1
-      .run<T, R>(taskData) // change input and output types to Pos[]
-      .then((taskResult: R | null) => setTaskResult(taskResult)); // change type from number to Pos[]
+  const runWorker = (taskData: T): void => {
+    worker1.run<T, R>(taskData).then(cb);
   };
 
-  return [taskResult, runWorker];
+  return runWorker;
 };
 
 export default useWorkerManager;
