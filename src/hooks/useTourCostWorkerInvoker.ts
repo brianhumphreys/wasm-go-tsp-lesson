5f3781e7deef0973c167502df537503193a2ca0a
Brian Humphreys <humbfrancis@gmail.com>
2022-04-20 08:11:28 -0500

    points have been hashed and runWorker now updates on new set of random points

--- src/hooks/useTourCostWorkerInvoker.ts
+++ src/hooks/useTourCostWorkerInvoker.ts
@@ -1,21 +1,14 @@
 import { useCallback } from "react";
 import { Pos } from "../types";
+import { hashListOfPoints } from "../utilities/pointUtils";
 import useWorkerManager from "./useWorkerManager";
 
-// change name but input/output remain the same as useVertexDistanceWorkerInvoker()
 const useTourCostWorkerInvoker = (input: Pos[], cb: (output: number | null) => void) => {
     const runWorker = useWorkerManager<Pos[], number>(cb);
 
     return useCallback(() => {
         runWorker(input);
-        // add length of points to the dependency array
-    // }, [input.length, runWorker]);
-    // todo: however, we see that if we click random, run, get a result,
-    // then click random again, the result printed to the console does not
-    // change.  this is because the length of the points array stayed at 100.
-    // We need to do something a little more clever like hasing the array 
-    // and listening to the hash of the array instead of just the length
-    }, [input.length, runWorker]);
+    }, [hashListOfPoints(input), runWorker]);
 }
 
 export default useTourCostWorkerInvoker;
