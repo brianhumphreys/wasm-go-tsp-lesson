9ce1a64d2a1032242c76a40f46ff457e5f1cea13
Brian Humphreys <humbfrancis@gmail.com>
2022-04-22 10:55:07 -0500

    cleanup go module

--- src/wasm_modules/main.go
+++ src/wasm_modules/main.go
@@ -16,85 +16,33 @@ type Tour struct {
 	cost     float64
 }
 
-func createDistanceMatrix(points []Vertex) map[Vertex]map[Vertex]float64 {
-	d := make(map[Vertex]map[Vertex]float64)
+// set global distance matrix instead of passing new matrix as result
+func createDistanceMatrixSetter(points []Vertex, distanceMatrix map[Vertex]map[Vertex]float64) {
     for i := range points {
-        d[points[i]] = make(map[Vertex]float64)
+        distanceMatrix[points[i]] = make(map[Vertex]float64)
     }
 
 	for i := 0; i < len(points); i++ {
 		for j := i; j < len(points); j++ {
 			if j == i {
-				d[points[j]][points[i]] = 0
+				distanceMatrix[points[j]][points[i]] = 0
 			} else {
 				dist := Distance(points[i], points[j])
-				d[points[j]][points[i]] = dist
-				d[points[i]][points[j]] = dist
-			}
-		}
-	}
-	return d
-}
-
-func createDistanceMatrixSetter(points []Vertex, state map[Vertex]map[Vertex]float64) {
-	// d := make(map[Vertex]map[Vertex]float64)
-    for i := range points {
-        state[points[i]] = make(map[Vertex]float64)
-    }
-
-	for i := 0; i < len(points); i++ {
-		for j := i; j < len(points); j++ {
-			if j == i {
-				state[points[j]][points[i]] = 0
-			} else {
-				dist := Distance(points[i], points[j])
-				state[points[j]][points[i]] = dist
-				state[points[i]][points[j]] = dist
+				distanceMatrix[points[j]][points[i]] = dist
+				distanceMatrix[points[i]][points[j]] = dist
 			}
 		}
 	}
 }
 
-func createDistanceMatrixInterfaceMap(points []Vertex) int {
-	// d := make(map[string]interface{})
-    // for i := range points {
-    //     inner := make(map[Vertex]float64)
-	// 	for j := 0; j < len(points); j++ {
-	// 		if j == i {
-	// 			inner[points[j]] = 0
-	// 		} else {
-	// 			dist := Distance(points[i], points[j])
-	// 			inner[points[j]] = dist
-	// 		}
-	// 	}
-	// 	d[points[i]] = inner
-    // }
-
-	
-	// test = test + 1
-	// d["yoink"] = map[string]int{"key1": 4}
-	return 7
-}
-
-func createDistanceMatrixWrapper(state map[Vertex]map[Vertex]float64) js.Func {
+func createDistanceMatrixWrapper(distanceMatrix map[Vertex]map[Vertex]float64) js.Func {
 	twoOptFunction := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		if len(args) != 1 {
 			return "Invalid number of arguments passed.  Expecting 1."
 		}
 		startPath := jsValueToVertexArray(args)
 
-	
-		createDistanceMatrixSetter(startPath, state)
-
-		// state = distanceMatrix
-		fmt.Println("in setter")
-		fmt.Println(state)
-		// out, err := json.Marshal(distanceMatrix)
-		// if err != nil {
-		// 	panic(err)
-		// }
-
-		// fmt.Println(out)
+		createDistanceMatrixSetter(startPath, distanceMatrix)
 
 		return 8
 	})
@@ -109,25 +57,6 @@ func pathCost(distanceMatrix map[Vertex]map[Vertex]float64, path []Vertex) float
 	total += distanceMatrix[path[len(path) - 1]][path[0]]
 	return total
 }
-
-// func twoOptWrapper() js.Func {
-// 	twoOptFunction := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-// 		if len(args) != 2 {
-// 			return "Invalid number of arguments passed.  Expecting 2."
-// 		}
-// 		startPath := jsValueToVertexArray(args)
-// 		distanceMatrix := createDistanceMatrix(startPath)
-
-// 		// handle situation when path is 1 or 0 points long
-// 		if len(startPath) == 0 || len(startPath) == 1 {
-// 			return vertexArrayToInterfaceMap(startPath) 
-// 		}
-// 		bestPath := otherTwoOpt(startPath, distanceMatrix)
-// 		return vertexArrayToInterfaceMap(bestPath)
-// 	})
-// 	return twoOptFunction
-// }
-
 
 func iterateTwoOpt(bestDistance float64, bestRoute []Vertex, distanceMatrix map[Vertex]map[Vertex]float64) (float64, []Vertex) {
 	newBestDistance := bestDistance
@@ -241,40 +170,13 @@ func Distance(vertex1 Vertex, vertex2 Vertex) float64 {
 	return math.Pow(math.Pow(vertex1.x-vertex2.x, 2)+math.Pow(vertex1.y-vertex2.y, 2), 0.5)
 }
 
-func oneWrapper(state map[Vertex]map[Vertex]float64) js.Func {
-	twoOptFunction := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if len(args) != 0 {
-			return "Invalid number of arguments passed.  Expecting 0."
-		}
-		fmt.Println(state)
-
-		return 0
-	})
-	return twoOptFunction
-}
-
-func twoWrapper(state map[Vertex]map[Vertex]float64) js.Func {
-	twoOptFunction := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if len(args) != 0 {
-			return "Invalid number of arguments passed.  Expecting 0."
-		}
-		// state[Vertex{x: 10, y: 12}] = state[Vertex{x: 10, y: 12}] + 1
-		fmt.Println(state)
-
-		return 0
-	})
-	return twoOptFunction
-}
 
 func main() {
 	// create global state that we pass to our function creators.  we want to keep
 	// state of the distance matrix so we can keep reference to it between iteration
 	// updates.
 	distanceMatrix := map[Vertex]map[Vertex]float64{}
-	js.Global().Set("two", twoWrapper(distanceMatrix))
-	js.Global().Set("one", oneWrapper(distanceMatrix))
   js.Global().Set("TwoOpt", twoOptWrapper(distanceMatrix)) // set the function
-//   js.Global().Set("DistMat", createDistanceMatrixWrapper()) // set the function
   js.Global().Set("DistMat", createDistanceMatrixWrapper(distanceMatrix)) // set the function
     <-make(chan bool)
 }
